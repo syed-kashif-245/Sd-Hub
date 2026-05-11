52cb20087128fa3b8e685ab435f9490217fa83e3
Syed Kashif <syedkashif2405@gmail.com>
2026-05-11 06:44:08 +0530

    updated sd-hub project

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,73 @@
 import React from 'react'
-import { Link } from 'react-router'
+import { NavLink } from 'react-router-dom'
 
 const Header = () => {
   return (
     <div className="flex items-center gap-96 px-10 py-4 mt-[20px] ml-[30px]">
 
-  <div className="">
-    <img
-      src="/sdhublogo.png"
-      className="w-[180px] h-[100px] object-contain "
-    />
-  </div>
+      <div>
+        <img
+          src="/sdhublogo.png"
+          className="w-[180px] h-[100px] object-contain"
+        />
+      </div>
 
-  <div className="flex gap-10  text-bold ">
-    <Link to="/" className=' text-blue-800 border-b-4 border-blue-800 pb-2'>Home</Link>
-    <Link to="about" className=' hover:text-blue-800 hover:border-b-4 hover:border-blue-800 pb-2'>About us</Link>
-    <Link to="/courses" className='hover:text-blue-800 hover:border-b-4 hover:border-blue-800 pb-2'>Courses</Link>
-    <Link to="/form" className='hover:text-blue-800 hover:border-b-4 hover:border-blue-800 pb-2'>Registration form</Link>
-   <Link to="/contact" ><button className='hover:text-blue-800 hover:border-b-4 hover:border-blue-800 pb-2'>
-  Contact Us<i className="fa-regular fa-message pl-2"></i></button>
-</Link>
-</div>
-</div>
+      <div className="flex gap-10 font-bold">
+
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive
+              ? "text-blue-800 border-b-4 border-blue-800 pb-2"
+              : "hover:text-blue-800 hover:border-b-4 hover:border-blue-800 pb-2 font-medium"
+          }
+        >
+          Home
+        </NavLink>
+
+        <NavLink
+          to="/about"
+          className={({ isActive }) =>
+            isActive
+              ? "text-blue-800 border-b-4 border-blue-800 pb-2"
+              : "hover:text-blue-800 hover:border-b-4 hover:border-blue-800 pb-2 font-medium"
+          }
+        >
+          About us
+        </NavLink>
+
+        <NavLink
+          to="/courses"
+          className={({ isActive }) =>
+            isActive
+              ? "text-blue-800 border-b-4 border-blue-800 pb-2"
+              : "hover:text-blue-800 hover:border-b-4 hover:border-blue-800 pb-2 font-medium"
+          }
+        >
+          Courses
+        </NavLink>
+
+        <a
+          href="https://bit.ly/sdhubreg"
+          className='hover:text-blue-800 hover:border-b-4 hover:border-blue-800 pb-2 font-medium'
+        >
+          Registration form
+        </a>
+
+        <NavLink
+          to="/contact"
+          className={({ isActive }) =>
+            isActive
+              ? "text-blue-800 border-b-4 border-blue-800 pb-2"
+              : "hover:text-blue-800 hover:border-b-4 hover:border-blue-800 pb-2 font-medium"
+          }
+        >
+          Contact Us
+          <i className="fa-regular fa-message pl-2"></i>
+        </NavLink>
+
+      </div>
+    </div>
   )
 }
 
--- src/components/Middle.jsx
+++ src/components/Middle.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { Link } from 'react-router'
 const middle = () => {
   return (
      <div className='mt-[130px] ml-[60px] flex'>
@@ -10,7 +10,7 @@ const middle = () => {
             <p className='text-5xl text-gray-800 font-extrabold text-gray-500'>Empowering Individuals</p>
             <p className='text-6xl text-blue-600 font-bold mt-5'>To Build Carriers</p> 
             <p className="text-gray-500 mt-5">Our ultimate goal is to equip individuals not only in core domain <br/>skills but also softskills and language training to thrive in the<br/> competitive job market.</p>
-            <button className='bg-blue-500 mt-5 px-5 py-2 text-white w-[160px]'> contact us </button>
+            <Link to="/contact" ><button className='bg-blue-500 mt-5 px-5 py-2 text-white w-[160px]'> contact us </button></Link>
         </div>
         <div className='ml-[100px]' >
             <img src='/2.jpg' className='h-[330px] w-[500px]'></img>
